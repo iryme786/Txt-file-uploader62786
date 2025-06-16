d01167b6ec80569e347fefb540032f5bc8316a07
iryme786 <manishamandall7777@gmail.com>
2025-06-16 19:05:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6535968101:AAFO4BnJlqGM5-EZ_h4XO7YXufbjsdq1JQw",
-             api_id= 20200825,
-             api_hash= "7e7f93e266c390d9cc5c2bffbe921e36")
+             bot_token= ":",
+             api_id= 29320367,
+             api_hash= "f8083ef747c160b7eb41095454f59f92")
 
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
